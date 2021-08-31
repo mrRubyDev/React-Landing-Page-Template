cf48abd8d56c1b0828de2cc02d9b16460c254354
MateoRubinstein <mateorubinstein@icloud.com>
2021-08-31 15:47:10 +0200

    Basari website personalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
-import { Features } from "./components/features";
 import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
+// import { Services } from "./components/services";
 import { Team } from "./components/Team";
 import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
@@ -13,29 +10,26 @@ import SmoothScroll from "smooth-scroll";
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
+	speed: 1000,
+	speedAsDuration: true,
 });
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+	const [landingPageData, setLandingPageData] = useState({});
+	useEffect(() => {
+		setLandingPageData(JsonData);
+	}, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+	return (
+		<div>
+			<Navigation />
+			<Header data={landingPageData.Header} />
+			<About data={landingPageData.About} />
+			{/* <Services data={landingPageData.Services} /> */}
+			<Team data={landingPageData.Team} />
+			<Contact data={landingPageData.Contact} />
+		</div>
+	);
 };
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,24 @@
-export const Header = (props) => {
-  return (
-    <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
-                  {props.data ? props.data.title : 'Loading'}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
-                <a
-                  href='#features'
-                  className='btn btn-custom btn-lg page-scroll'
-                >
-                  Learn More
-                </a>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  )
-}
+export const Header = props => {
+	return (
+		<header id="header">
+			<div className="intro">
+				<div className="overlay">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-8 col-md-offset-2 intro-text">
+								<h1>
+									{props.data ? props.data.title : "Loading"}
+									<span></span>
+								</h1>
+								<p>{props.data ? props.data.paragraph : "Loading"}</p>
+								<a href="#about" className="btn btn-custom btn-lg page-scroll">
+									Saber más
+								</a>{" "}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
